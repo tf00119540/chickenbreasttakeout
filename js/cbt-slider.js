48b06171927312add9c065e859cc12564c22a90d
kingyi <tf00119540@gmail.com>
2023-02-22 15:36:51 +0800

    'add'

---
+++ js/cbt-slider.js
@@ -0,0 +1,7 @@
+        $(document).ready(function () {
+            $('.slider').bxSlider({
+                auto: true,
+                speed: 800,
+                autoHover: true
+            });
+        });
